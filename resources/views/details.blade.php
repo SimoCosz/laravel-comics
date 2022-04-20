9bceee68e0a5db286d51c474def5a1bcb75a1dd0
coszach@hotmail.it <coszach@hotmail.it>
2022-04-20 16:00:17 +0200

    print prima descrizione comic

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,34 @@
 @extends('partials.standard')
 
 @section('mainContent')
-@dump($comic)
+<div class="poster-top">
+  <figure class="container">
+    <img src="{{$comic['thumb']}}" alt="">
+  </figure>
+</div>
+<div class="container comic__container">
+  <section class="comic-description">
+    <h1 class="comic__title">{{$comic['title']}}</h1>
+    <div class="comic__availability">
+      <div class="comic__price">
+        U.S. Price <span>{{$comic['price']}}</span>
+      </div>
+      <div class="comic__status">available</div>
+      <select class="check" name="" id="" placeholder='Check Availability'>
+        <option value="">Check Availability</option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+    </div>
+    <p class="comic__text">{{$comic['description']}}</p>
+  </section>
+
+  <div class="advertisement">
+    advertisement
+    <figure>
+      <img src="{{asset('img/adv.jpg')}}" alt="">
+    </figure>
+  </div>
+</div>
 @endsection
 
